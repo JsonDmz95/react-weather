637e0217e01812f84d8027caab8d8c9a7f66edff
Jeeson Domiguez <dominguez_js@outlook.com>
2020-07-23 00:44:41 -0600

    Updates

---
+++ precache-manifest.51a242c06b41d5ae2af3bcc36c117514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4a828ac3098f54b56f4573194c570a",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "be76092c39b40607081f",
+    "url": "/react-weather/static/css/main.4d671195.chunk.css"
+  },
+  {
+    "revision": "bc1cabacc8cb88bf9cf4",
+    "url": "/react-weather/static/js/2.903f7480.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.903f7480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be76092c39b40607081f",
+    "url": "/react-weather/static/js/main.2e2d0039.chunk.js"
+  },
+  {
+    "revision": "f62618312724e8c367b8",
+    "url": "/react-weather/static/js/runtime-main.ee67f24b.js"
+  }
+]);
